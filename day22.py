201964d1ebcca3f7a3892de80e37e0e4573d5998
Matt Pang <matt.pang@tomoro.ai>
2026-01-07 07:40:27 +0000

    finished day 22

--- day22.py
+++ day22.py
@@ -50,6 +50,61 @@ def run(n):
     return infected
     
 
+def run_pt2(n,debug=False):
+    grid = dict()
+    
+    for y,line in enumerate(d.splitlines()):
+        for x, c in enumerate(line):
+            if c == '#':
+                grid[complex(x,y)] = c
+                
+    c =  len(d.splitlines()) // 2
+    pos = complex(c,c)
+    facing = complex(0,-1)
+    
+    infected = 0 
+    for i in range(n):
+        if grid.get(pos) is None:
+            # turn left
+            grid[pos] = 'W'
+            facing *= complex(0,-1)
+            pos += facing
+        elif grid.get(pos) == '#':
+            # turn right
+            grid[pos] = 'F'
+            facing *= complex(0,1)
+            pos += facing
+        elif grid.get(pos) == 'W':
+            grid[pos] = '#'
+            infected +=1
+            pos += facing
+        elif grid.get(pos) == 'F':
+            # reflects if flagged
+            del grid[pos]
+            facing *= complex(0,1)
+            facing *= complex(0,1)
+            pos += facing
+
+        if debug:
+            xs = [int(x.real) for x in grid.keys()]
+            ys = [int(x.imag) for x in grid.keys()]
+        
+            for y in range(min(ys)-1,max(ys)+2):
+                line = ''
+                for x in range(min(xs)-1,max(xs)+2):
+                    c = grid.get(complex(x,y),'.')
+                    if pos == complex(x,y):
+                        c = 'X'
+                    line+=c
+                print(line)
+            print('---')    
+        
+
+    return infected
+
 # assert run(70) == 41
 # assert run(10000) == 5587
-run(10000)
+# assert run_pt2(7,True)== 26
+# assert run_pt2(10000000) == 2511944
+print(run(10000))
+print(run_pt2(10000000))
